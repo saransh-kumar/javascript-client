35979e5b2647f7e0898a3d02d780988afb08d54e
Saransh Kumar <72736644+saransh-kumar@users.noreply.github.com>
2020-12-15 19:56:34 +0530

    docs: add Maths Theme theme.js

--- training/src/App.js
+++ training/src/App.js
@@ -1,13 +1,15 @@
 // import { TextFieldDemo } from './pages/index';
-import { InputDemo } from '../src/pages/index';
+// import { InputDemo } from '../src/pages/index';
+import { ChildrenDemo } from './pages/ChildrenDemo/index';
 
 import React from 'react';
 
 function App() {
   return (
     <>
+      <ChildrenDemo />
       {/* <TextFieldDemo /> */}
-      <InputDemo />
+      {/* <InputDemo /> */}
     </>
   );
 }
