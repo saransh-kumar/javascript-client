fd4098c5f887788e0efcf2dc41b73f80ff361bc4
Saransh Kumar <72736644+saransh-kumar@users.noreply.github.com>
2021-01-31 20:51:31 +0530

    feat: crud using apollo

---
+++ training/src/pages/Trainee/components/AddDialog/Wrapper.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Mutation } from '@apollo/react-components';
+import { CREATE_USER } from './Mutation';
+import AddDialog from './AddDialog';
+
+export default class Wrapper extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const prop = this.props;
+    return (
+      <Mutation mutation={CREATE_USER}>
+        {(createTrainee) => (
+          <>
+            <AddDialog createTrainee={createTrainee} {...prop} />
+          </>
+        )}
+      </Mutation>
+    );
+  }
+}
