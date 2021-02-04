cd8659ad0c8f04b0e91953ed6a8d6ae6d217b634
Saransh Kumar <72736644+saransh-kumar@users.noreply.github.com>
2021-02-04 18:25:52 +0530

    feat: subscribtion edit delete

--- training/src/lib/apollo-client.js
+++ training/src/lib/apollo-client.js
@@ -1,11 +1,21 @@
 /* eslint-disable array-callback-return */
-/* eslint-disable no-alert */
+/* eslint-disable */
 import { InMemoryCache } from 'apollo-boost';
 import { ApolloClient } from '@apollo/client';
+import { split } from 'apollo-link';
 import { HttpLink } from 'apollo-link-http';
 import { setContext } from '@apollo/client/link/context';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
 
-const link = new HttpLink({ uri: 'http://localhost:9000/graphql' });
+const httpLink = new HttpLink({ uri: 'http://localhost:9000/graphql' });
+
+const wsLink = new WebSocketLink({
+    uri: 'ws://localhost:9000/graphql',
+    options: {
+        reconnect: true, 
+    }
+})
 
 const authLink = setContext((_, { headers }) => {
     const token = localStorage.getItem('token');
@@ -16,6 +26,18 @@ const authLink = setContext((_, { headers }) => {
         },
     };
 });
+
+const link = split(
+    ({ query }) => {
+        const definition = getMainDefinition(query);
+        return (
+            definition.kind === 'OperationDefinition'
+            && definition.operation === 'subscription'
+        );
+    }, 
+    wsLink,
+    httpLink,
+);
 
 const apolloclient = new ApolloClient({
 cache: new InMemoryCache(),
